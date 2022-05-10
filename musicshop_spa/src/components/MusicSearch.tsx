0df38b410d409152f7f43697744c9a7c865f164c
Adrian <adrian.essig@outlook.com>
2022-05-10 15:21:03 +0200

    Changes by marco 🏴‍☠️🥚

--- musicshop_spa/src/components/MusicSearch.tsx
+++ musicshop_spa/src/components/MusicSearch.tsx
@@ -1,9 +1,81 @@
 import React, {Component} from 'react';
-import SideNavbar from './SideNavbar';
-import TextField from '@mui/material/TextField';
 
-class MusicSearch extends Component {
+import {TextField, Button, Grid, Divider} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import {DefaultApi} from "../openAPI";
+import ProductCard from "./ProductCard";
+import {AlbumDTO} from "../openAPI";
+
+class MusicSearch extends Component<{}, { albumDTOs: Set<AlbumDTO> }> {
+
+    private searchString: string;
+    private defaultApi: DefaultApi;
+
+
+    constructor(props: any) {
+        super(props);
+
+        this.searchString = "";
+        this.defaultApi = new DefaultApi();
+
+        this.state = {
+            albumDTOs: new Set(),
+        }
+    }
+
+    private searchMusic(): void {
+        this.defaultApi.findAlbumsBySongTitle(this.searchString).then(
+            success => {
+                if (success == null || success.data == null) {
+                    console.log("Ohje.. 🐜");
+                    return;
+                }
+
+                let albumDTOs = new Set<AlbumDTO>();
+
+                for (let i = 0; i < success.data.length; i++) {
+                    albumDTOs.add(success.data[i]);
+                }
+
+                this.setState({albumDTOs: albumDTOs});
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+    private searchBarContent() {
+        return (
+            <Grid container justifyContent={"flex-start"} sx={{mb: 3}}>
+                <TextField
+                    size={"small"}
+                    id={"music-search-input"}
+                    label={"Search..."}
+                    variant={"filled"}
+                    style={{width: "30%", minWidth: 300}}
+                    onChange={(e) => {
+                        this.searchString = e.target.value;
+                    }}
+                />
+
+                <Button
+                    sx={{ml: 3}} variant={"contained"}
+                    onClick={() => {
+                        this.searchMusic();
+                    }}
+                    startIcon={<SearchIcon/>}
+                >
+                    Search
+                </Button>
+            </Grid>
+        );
+    }
+
     render() {
+
+        const {albumDTOs} = this.state;
+
         return (
             <div>
                 {/*<Container >*/}
